8feaf55a3f8637be6c433f8c2955ce9737859d5e
digmunde <digvijay.munde@gmail.com>
2025-07-17 16:31:07 +0530

    first commit

---
+++ src/main/java/com/jenkineg/demo/SbJenkinEgApplication.java
@@ -0,0 +1,13 @@
+package com.jenkineg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJenkinEgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJenkinEgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkineg/demo/SbJenkinEgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkineg.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJenkinEgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
